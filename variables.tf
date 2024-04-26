ef01bb15c62d3b4ca36a36363cbf4f9a03793d3f
Mark Chapman <41584964+MarkChapmanBede@users.noreply.github.com>
2024-04-26 16:06:07 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "Pubkey"
+  type        = string
+  default     = ""
+}
