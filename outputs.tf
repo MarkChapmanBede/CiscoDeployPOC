3562f30894b5e5a02a1631a5f3ac8025d2c93a7c
Mark Chapman <41584964+MarkChapmanBede@users.noreply.github.com>
2024-05-01 14:31:19 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "asa_vm_public_ip" {
-  value = azurerm_public_ip.asa_public_ip_outside.ip_address
+output "asa_vm_public_ips" {
+  value = azurerm_public_ip.asa_public_ip_outside[*].ip_address
 }
